e96a92bb79b29bc028b4e241918049670c4e679c
jvcalassio <joao.victors1203@gmail.com>
2019-05-01 00:40:38 -0300

    Caixeiro DP com path

--- Lab1/caixeiro.cpp
+++ Lab1/caixeiro.cpp
@@ -8,12 +8,9 @@ using namespace std;
 /*
 Resumo:
 Entregador de pizza vai da casa 0 ate todas as casas
-Sub caminhos entre casas ja visitadas armazenado na matriz de visitados
-Assim, reduz tempo de sub execucoes
+Subcaminhos entre casas ja visitadas armazenado na matriz de caminhos visitados (dp)
+Assim, reduz tempo de subexecucoes
 No fim, volta a casa 0
-
-* Fazer modificacao para funcao manual de permutacoes (sem uso da STL) 
-* Desnecessario. Funcao com dp mais rapida e mais facil de implementar
 
 Utiliza bitmask para simular as casas ja visitadas na dfs
 Sendo S[6] = { S[0], S[1], S[2], S[3], S[4], S[5], S[6] }, com S[i] = 0 ou 1, indicando se foi visitado ou nao
@@ -27,8 +24,8 @@ int distancias[][N] = {  {0,10,20,30,40,20},
 						 {40,12,14,11,0,10},
 					 	 {20,10,17,9,10,0}};
 
-// visitados com N x 2 ^ N posicoes
-int visitados[N][1 << N];
+// caminhos visitados com N x 2 ^ N posicoes
+int dp[N][1 << N];
 // matriz auxiliar para salvar o caminho percorrido. N x 2^N posicoes tbm
 int savePath[N][1 << N];
 
@@ -49,26 +46,16 @@ bool completou(int S){
 	return S == (1 << N)-1;
 }
 
-void binprintf(int v)
-{
-    unsigned int mask=1<<((1<<3)-1);
-    while(mask) {
-        printf("%d", (v&mask ? 1 : 0));
-        mask >>= 1;
-    }
-}
-
 // v = vertice atual
 // S = bitmask de casas visitadas
 int caixeiro(int v, int S){
 	if(completou(S))
 		return distancias[v][0];
 
-	if(visitados[v][S] != -1)
-		return visitados[v][S];
+	if(dp[v][S] != -1)
+		return dp[v][S];
 
 	int menor_caminho = 1e9;
-	int chosen_i = 0;
 	for(int i=0;i<N;i++){
 		if(!visitada(i, S)){
 			int q = caixeiro(i, add_visitadas(i, S)) + distancias[v][i];
@@ -78,38 +65,12 @@ int caixeiro(int v, int S){
 			}
 		}
 	}
-	visitados[v][S] = menor_caminho;
+	dp[v][S] = menor_caminho;
 	return menor_caminho;
 }
-
-
-/*
-int caixeiro(){
-	int menor_caminho = 1e9;
-	int casas[] = {1,2,3,4,5};
-	// permutar casas
-	next_permutation(casas, casas+(N-1));
-
-	int qtd_perm = fat(N-1);
-
-	do {
-		int custo_atual = 0;
-		int k = 0;
-		for(int i=0;i<(N-1);i++){
-			custo_atual += caminhos[k][casas[i]];
-			k = casas[i];
-		}
-		custo_atual += caminhos[k][0];
-		menor_caminho = min(menor_caminho, custo_atual);
-		next_permutation(casas, casas+(N-1));
-		qtd_perm--;
-	} while(qtd_perm > 0);
-	return menor_caminho;
-}
-*/
 
 int main(){
-	memset(visitados, -1, sizeof(visitados));
+	memset(dp, -1, sizeof(dp));
 	printf("Distancia: %d\n", caixeiro(0, 1));
 	
 	int casa_atual = 0;
