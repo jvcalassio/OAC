456449d504a08b502271f0b225d13016702d256f
jvcalassio <joao.victors1203@gmail.com>
2019-05-08 18:13:24 -0300

    caixeiro

--- Lab1/Lab1_3.s
+++ Lab1/Lab1_3.s
@@ -5,20 +5,11 @@
 	sw %reg,0(sp)
 .end_macro
 
-.macro save_stackf(%reg)
-	addi sp,sp,-4
-	fsw %reg,0(sp)
-.end_macro
-
 .macro free_stack(%reg)
 	lw %reg,0(sp)
 	addi sp,sp,4
 .end_macro
 
-.macro free_stackf(%reg)
-	flw %reg,0(sp)
-	addi sp,sp,4
-.end_macro
 
 .macro get_pointer(%n,%vet,%y,%x) # nao usar t0 como ponteiro para o vet !
 	save_stack(t0)
@@ -31,14 +22,13 @@
 
 .data
 C: .space 160 # alocar 160 espacos na memoria 2 coord(x,y) * max casas (20) * 4 bytes cada coord
-N: .word 4 # numero de casas
+N: .word 9 # numero de casas
 D: .space 1600 # matriz das distancias entre as casas 20 * 20 * 4 bytes de tamanho max
 
-dp: .space 0xa000 # caminhos visitados 20 casas max * 2^10 bits (bitmask, 1 bit = 1 casa) * 4 bytes
-savepath: .space 0xa000 # matriz auxiliar para salvar caminho utilizado 20 casas max * 2^20 bits (bitmask)
-			   # nao precisa ser word, salva valores de 0 a 20 apenas
-			   # talvez mude pela comodidade de usar mesmo contador da dp
-printvet: .space 160 
+vet_permut: .space 80 # vetor dos numeros das casas (para permutar)
+vet_permut_final: .space 80 # vetor dos numeros das casas na melhor ordem
+
+printvet: .space 160 # vetor a ser impresso (C ordenado)
 
 next_line: .string "\n"
 blank_line: .string " "
@@ -46,10 +36,6 @@ minus1float: .float -1
 
 .text
 	M_SetEcall(exceptionHandling)
-	
-	#la a0,savepath
-	#li a7,34
-	#ecall
 	
 	la t0,N
 	lw a0,0(t0)
@@ -79,10 +65,7 @@ minus1float: .float -1
 	la a2,D
 	jal ORDENA
 	
-	#la t0,N
-	#lw a0,0(t0)
-	#jal PRINT_SAVEPATH
-	
+	fmv.s fa0,fs0
 	li a7,2
 	ecall # printa min_dists
 	la a0,next_line
@@ -91,11 +74,14 @@ minus1float: .float -1
 	
 	jal PRINTORDENADOS
 	
-	#la t0,N
-	##lw a0,0(t0)
-	#jal PRINT_REDLINE
+	la t0,N
+	lw a0,0(t0)
+	la a1,C
+	jal MAKE_PRINTVET
 	
-	#jal PRINT_C_VET
+	la t0,N
+	lw a0,0(t0)
+	jal PRINT_REDLINE
 	
 	j FIMPROG
 
@@ -227,6 +213,7 @@ ROTAS:
 	FIM_LOOP_CASAS:
 		ret
 
+# TEMPORARY:
 PRINT_MATRIZ:
 	mv s0,a0 # s0 = n
 	mv s1,a1 # s1 = matriz a ser impressa
@@ -275,281 +262,187 @@ ORDENA:
 	mv s1,a1 # a1 = C
 	mv s2,a2 # a2 = D
 	
-	# seta a matriz de dp para -1, em todos os valores
-	RESETA_VISITADOS:
-		li t0,0 # i = 0
-		la t1,dp # ponteiro para matriz
-		li t3,1
-		sll t3,t3,s0 # 1 << n (shifta 1 para esquerda n vezes)
-		mul t3,t3,s0 # t = n * (1 << n) = numero de posicoes na matriz com as distancias totais
-		
-		LOOP_RESET:
-			beq t0,t3,PRE_CAIXEIRO # se i == t, toda a matriz tem valores -1
-			#li t2,-1
-			la t2,minus1float
-			lw t2,0(t2)
-			sw t2,0(t1) # armazena -1 na posicao especificada
-			
-			addi t1,t1,4 # incrementa endereco
-			addi t0,t0,1 # incrementa i 
-			j LOOP_RESET
-			
-	# aqui comeca a magica
-	# a casa inicial eh sempre a casa 0
-	# casa atual = a0, comecando em 0
-	# bitmask das casas visitadas = a1, inicialmente 0000001
-	# se caminho ja foi feito, pegar posicao na matriz (dp)
-	# se caso nao foi feito, calcular entre todas as possibilidades, qual o menor caminho
-	# apos finalizar, pegar pontos na matriz de path e desenhar no bitmap
 	PRE_CAIXEIRO:
-		li a0,0
-		li a1,1
+		li t0,0 # i = 0
+		la t1,vet_permut # ponteiro inicial do vetor
+		LOOP_SETVET: # seta o vetor inicial de 1 a N (para permutar)
+			beq t0,s0,FIM_LOOP_SETVET
+			sw t0,0(t1)
+			addi t0,t0,1
+			addi t1,t1,4
+			j LOOP_SETVET
+		
+	FIM_LOOP_SETVET:
+	# global: fs0 = menor custo
+	li t0,10000
+	fcvt.s.w fs0,t0
+	li a0,0
+	j PERMUTA
+	# faz o calculo de melhor rota, testando todas as possibilidades
 	
-	CAIXEIRO:
-		# a0 = v
-		# a1 = S
-		save_stack(a0)
-		save_stack(a1)
-		li t0,1
-		sll t0,t0,s0 # (1 << N)
-		addi t0,t0,-1 # (1 << N)-1
-		
-		# prepara retorno em fa0. carrega D[v][0]
-		mul t1,s0,a0 # t1 = (n * v)
-		slli t1,t1,2 # t1 = (n * v * 4 bytes)
-		add t1,s2,t1 # t1 = end de D[v][0]
-		flw fa0,0(t1) # fa0 = D[v][0]
-		beq a1,t0,RETURN_RA # se S == ((1 << N)-1)
-		
-		# se nao entrar no primeiro if, verificar se dp ja foi feita
-		la t1,dp
-		get_pointer(s0,t1,a0,a1) # pega o ponteiro de dp[v][S] e retorna no reg do end.
-		# t1 = endereco para dp[v][S]
-		flw fa0,0(t1) # fa0 = dp[v][S]
-		la t0,minus1float
-		flw ft0,0(t0) # ft0 = -1 (float)
-		feq.s t0,ft0,fa0 # t0 = fa0 == ft0 (dp[v][S] == -1)
-		beq t0,zero,RETURN_RA # se t0 == 0 (ou seja, fa0 != -1), retorna
-		
-		# se nao entrar em nenhum dos ifs, calcula o melhor caminho
-		li t0,10000
-		fcvt.s.w fs0,t0 # fs0 = menor caminho. numero grande inicialmente
-		li s3,0 # contador i = 0
-		LOOP_CALCROTA:
-			beq s3,s0,FIMLOOP_CALCROTA # se i == n, fim do loop
-			li t0,1
-			sll t0,t0,s3 # t0 = (1 << i)
-			and t0,a1,t0 # t0 = (S & (1 << i)). se casa nao visitada, t0 = 0
-			bne t0,zero,CONTLOOP_CALCROTA # se casa ja foi visitada, pula pra prox
-				# calcular caixeiro(i,(S | 1 << i)
-				# calcular D[v][i]
-				# somar, ver se menor caminho > q = ret + D[v][i]
-				save_stack(a0)
-				save_stack(a1)
-				save_stackf(fs0)
-				save_stack(ra)
-				save_stack(s3) # salva registradores importantes na pilha
-				
-				mv a0,s3 # coloca i atual no primeiro arg (v)
-				li t0,1
-				sll t0,t0,s3 # t0 = (1 << i)
-				or a1,a1,t0 # coloca (S | (1<<i) no segundo arg (S)
-				jal CAIXEIRO # chama a funcao recursivamente
-				
-				# ao retornar da funcao, o retorno estara em fa0
-				free_stack(s3)
-				free_stack(ra)
-				free_stackf(fs0)
-				free_stack(a1)
-				free_stack(a0) # retorna valores da pilha
-				
-				mv t1,s2 # t1 = D
-				get_pointer(s0,t1,a0,s3) # pega end de D[v][i]
-				# t1 = end de D[v][i]
-				flw ft0,0(t1) # ft0 = D[v][i]
-				fadd.s ft0,ft0,fa0 # ft0 = D[v][i] + ret da funcao recursiva
-				
-				# verificar se eh menor que o menor caminho
-				flt.s t0,ft0,fs0 # t0 = (ft0 < fs0)
-				beq t0,zero,CONTLOOP_CALCROTA # se (ft0 > fs0), so continua e nao muda menor_caminho
-					# do contrario, muda valor salvo em menor_caminho
-					fmv.s fs0,ft0 # fs0 = ft0
+	# sem argumentos. apenas testa se a permutacao atual tem a menor rota e seta fs0
+	TESTA_ROTA:
+		fcvt.s.w fs1,zero # soma = 0
+		li s4,0 # i = 0, contador
+		LOOP_SOMADIST: # loop para somar distancias
+			beq s4,s0,FIM_LOOPSOMADIST
+			mv t1,s2
+			rem t2,s4,s0 # t2 = i%n
+			addi t3,s4,1 # t3 = i+1
+			rem t3,t3,s0 # t3 = (i+1)%n
+			la t4,vet_permut
+			slli t2,t2,2 # t2 * 4 bytes
+			add t2,t4,t2 # a[i%n]
+			slli t3,t3,2 # t3 * 4 bytes
+			add t3,t4,t3 # a[(i+1%n]
+			lw t2,0(t2)
+			lw t3,0(t3)
+			
+			get_pointer(s0,t1,t2,t3) # ponteiro colocado em t1
+			flw ft0,0(t1)
+			fadd.s fs1,fs1,ft0 # soma += D[t2][t3]
+			
+			addi s4,s4,1
+			j LOOP_SOMADIST
+			
+		FIM_LOOPSOMADIST:
+			# apos finalizar o loop, testar se soma eh menor q o menor custo atual
+			flt.s t0,fs1,fs0 # t0 = (fs0 > fs1)
+			beq t0,zero,RET_TESTAROTA # se o salvo for menor, soh retorna
+				# do contrario, seta novo menor
+				fcvt.s.w ft0,zero
+				fadd.s fs0,fs1,ft0 # fs0 = fs1
+				# salva melhor ordem no vetor de final
+				li t0,0 # i = 0
+				la t1,vet_permut
+				la t2,vet_permut_final
+				LOOP_SAVEBEST:
+					beq t0,s0,RET_TESTAROTA
+					lw t3,0(t1)
+					sw t3,0(t2)
 					
-					# faz savepath[v][S] = i
-					la t1,savepath
-					get_pointer(s0,t1,a0,a1) # pega ponteiro de savepath[v][S]
-					sw s3,0(t1) # salva o i em savepath[v][S]
+					addi t1,t1,4 # incrementa ponteiro
+					addi t2,t2,4 # incrementa ponteiro
+					addi t0,t0,1 # incrementa i
+					j LOOP_SAVEBEST
 			
-			CONTLOOP_CALCROTA: # iterador do loop
-				addi s3,s3,1
-				j LOOP_CALCROTA
-			
-		FIMLOOP_CALCROTA:
-			# ao acabar o loop, salvar novo menor caminho em dp[v][S]
-			free_stack(a1)
-			free_stack(a0)
-			la t1,dp
-			get_pointer(s0,t1,a0,a1) # t1 = ponteiro para dp[v][S]
-			fsw fs0,0(t1) # salva menor caminho em dp[v][S]
-			fmv.s fa0,fs0 # return = menor_caminho
+		RET_TESTAROTA:
 			ret
 	
-	RETURN_RA:
-		free_stack(a1)
-		free_stack(a0)
-		# retorna fa0 = menor distancia
+		
+	# permuta o vetor de casas (valores apenas, sem xy)
+	# argumentos:
+	# a0 = casa atual
+	PERMUTA:
+		addi t0,s0,-1 # t0 = (N-1)
+		beq a0,t0,CALL_TESTAROTA # se casa atual == casa N-1 (ja percorreu todas), testa rota
+			mv s4,a0 # i = a0
+			LOOP_PERMUTA:
+				beq s4,s0,FIM_LOOP_PERMUTA # se i == N, sai
+					la t0,vet_permut
+					slli t1,a0,2
+					add t0,t0,t1 # vet_permut + casa atual
+					
+					la t1,vet_permut
+					slli t2,s4,2
+					add t1,t1,t2 # vet_permut + j
+					lw t2,0(t0) # carrega valor de vet_permut[casaatual]
+					lw t3,0(t1) # carreag valor de vet_permut[j]
+					sw t2,0(t1)
+					sw t3,0(t0) # troca valores de posicao
+					
+					save_stack(a0)
+					save_stack(s4)
+					save_stack(t0)
+					save_stack(t1)
+					save_stack(ra)
+					addi a0,a0,1 # novo argumento: casa atual + 1
+					jal PERMUTA
+					
+					# refaz swap
+					free_stack(ra)
+					free_stack(t1)
+					free_stack(t0)
+					free_stack(s4)
+					free_stack(a0)
+					lw t2,0(t0) # valor de vet_permut[casaatual]
+					lw t3,0(t1) # valor de vet_permut[j]
+					sw t2,0(t1)
+					sw t3,0(t0) # troca valores de posicao
+					
+					addi s4,s4,1
+					j LOOP_PERMUTA
+		
+		CALL_TESTAROTA:
+			save_stack(ra)
+			jal TESTA_ROTA
+			free_stack(ra)
+			
+	FIM_LOOP_PERMUTA:
 		ret
 
-PRINT_SAVEPATH:
-	mv s0,a0 # s0 = n
-	la s1,savepath
-	
-	# posicoes: savepath[n][1 << n]
-	# iterar n * (1 << n) vezes. a cada N vezes, printar \n
-	li s5,1
-	sll s5,s5,s0 # 1 << n
-	mul s2,s5,s0 # N * (1 << n) vezes. max iterations
-	
-	li s3,0 # i = 0
-	li s4,0 # j = 0
-	LOOP1_SPP:
-		beq s3,s2,END_PRINT
-		bne s4,s5,DONTPRINTNL
-			la a0,next_line
-			li a7,4
-			ecall
-			li s4,0
-		DONTPRINTNL:
-			lw a0,0(s1)
-			li a7,1
-			ecall
-			
-			la a0,blank_line
-			li a7,4
-			ecall
-			
-			addi s1,s1,4
-			addi s3,s3,1
-			addi s4,s4,1
-			j LOOP1_SPP
-	
-	END_PRINT:
-		ret
-
+# TEMPORARY:
+# printa casas na melhor ordem		
 PRINTORDENADOS:
-	la t0,N
-	lw s0,0(t0) # s0 = n
-	la s1,savepath # s1 = savepath
+	la s0,N
+	lw s0,0(s0) # s0 = n
+	la s1,vet_permut_final # s1 = vetor ordenado na melhor ordem
 	
-	li s2,0 # casa atual
-	li s3,1 # bitmask = 000001
-	li s4,0 # i = 0
-	
-	la s5,C
-	la s6,printvet
-	
-	lw t0,0(s5) # x0
-	lw t1,4(s5) # y0
-	sw t0,0(s6)
-	sw t1,4(s6)
-	addi s6,s6,8 # inicia em printvet[1]
-	
+	li s2,0 # i = 0
 	LOOPPO:
-		#mul t0,s0,s2 # t0 = casa atual * n
-		#add t0,t0,s3 # t0 = (casa atual*n) + bitmask
-		#slli t0,t0,2
-		#add t0,s1,t0 # t0 = end de savepath[casa_atual][bitmask]
-		mv t1,s1
-		get_pointer(s0,t1,s2,s3)
-		mv t0,t1
-		
-		lw s2,0(t0)
-		lw a0,0(t0)
-		li a7,1
-		ecall 
-		
-		# pegar (s2)esima casa de C, e salvar no printvet
-		slli t1,s2,3
-		add t0,s5,t1 # c + (s2 * 4 bytes)
-		lw t1,0(t0)
-		sw t1,0(s6) # salva x em printvet
-		lw t1,4(t0)
-		sw t1,4(s6) # salva y em printvet
-		
-		la a0,blank_line
-		li a7,4
-		ecall
-		
-		li t1,1
-		sll t1,t1,s2 # t1 = (1 << casa_atual)
-		or s3,s3,t1 # incrementa bitmask
-		addi s6,s6,8 # incrementa printvet
-		
-		addi s4,s4,1 # incrementa contador
-		bne s4,s0,LOOPPO
-		
-	VOLTAPO:
-		ret
-
-PRINT_C_VET:
-	li s0,6
-	la s1,C
-	li s2,0
-	la s3,printvet
-	
-	LOOPCVET:
-		beq s2,s0,FIMLOOPCVET
+		beq s2,s0,VOLTAPO
 		lw a0,0(s1)
 		li a7,1
-		ecall
-		
+		ecall # printa num
 		la a0,blank_line
 		li a7,4
-		ecall
+		ecall # printa espaco
 		
-		lw a0,4(s1)
-		li a7,1
-		ecall
-		
-		la a0,next_line
-		li a7,4
-		ecall
-		
+		addi s1,s1,4
 		addi s2,s2,1
-		addi s1,s1,8
-		j LOOPCVET
-		
-	FIMLOOPCVET:
+		j LOOPPO
+	
+	VOLTAPO:
 		la a0,next_line
 		li a7,4
 		ecall
-		li s2,0
+		ret
 		
-	LOOP2CVET:
-		beq s2,s0,FIM2LOOPCVET
-		lw a0,0(s3)
-		li a7,1
-		ecall
+####################################################
+#  Faz vetor para utilizar na impressao das linhas
+# a0 = numero de casas
+# a1 = vetor C de casas
+####################################################
+MAKE_PRINTVET:
+	mv s0,a0 # s0 = n
+	mv s1,a1 # s1 = C
+	la s2,vet_permut_final
+	la s3,printvet
+	
+	li s4,0 # i = 0
+	LOOP_MAKEPV:
+		beq s4,s0,FIM_MAKEPV
+		lw t0,0(s2) # carrega qual a i-esima casa
+		slli t0,t0,3 # t0 * 8 bytes (pelo x,y) para procurar a i-esima casa em C
+		add t0,s1,t0 # t0 = endereco da i-esima casa em C
 		
-		la a0,blank_line
-		li a7,4
-		ecall
+		lw t1,0(t0) # Xi
+		lw t2,4(t0) # Yi
+		sw t1,0(s3)
+		sw t2,4(s3) # grava Xi e Yi no printvet
 		
-		lw a0,4(s3)
-		li a7,1
-		ecall
-		
-		la a0,next_line
-		li a7,4
-		ecall
-		
-		addi s2,s2,1
 		addi s3,s3,8
-		j LOOP2CVET
-		
-	FIM2LOOPCVET:
+		addi s4,s4,1
+		addi s2,s2,4
+		j LOOP_MAKEPV
+
+	FIM_MAKEPV:
 		ret
 
+################################################
+# Desenha as linhas vermelhas no bitmap
+# a0 = N
+################################################
 PRINT_REDLINE:
 	mv s0,a0 # s0 = n
 	addi s0,s0,-1 # so faz ate o penultimo. o ultimo se liga no primeiro
@@ -563,7 +456,7 @@ PRINT_REDLINE:
 		lw a1,4(s1) # y1
 		lw a2,8(s1) # x2
 		lw a3,12(s1) # y2 
-		li a4,0x0F
+		li a4,0x07
 		li a5,0
 		li a7,147
 		ecall
@@ -578,7 +471,7 @@ PRINT_REDLINE:
 		la s1,printvet
 		lw a2,0(s1)
 		lw a3,4(s1)
-		li a4,0x0F
+		li a4,0x07
 		li a5,0
 		li a7,147
 		ecall
--- Lab1/caixeiro.cpp
+++ Lab1/caixeiro.cpp
@@ -1,115 +1,57 @@
 #include <iostream>
-#include <cstring>
 
 using namespace std;
 
-#define N 5
+#define N 6
 
-/*
-Resumo:
-Entregador de pizza vai da casa 0 ate todas as casas
-Subcaminhos entre casas ja visitadas armazenado na matriz de caminhos visitados (dp)
-Assim, reduz tempo de subexecucoes
-No fim, volta a casa 0
+int a[] = {0, 1, 2, 3, 4, 5};
+int best[N];
+int c = 0, cost = 1e9;
+int distancias[][N] = {{0, 192, 116, 26, 84, 184},
+                       {192, 0, 122, 101, 9, 40},
+                       {116, 122, 0, 21, 171, 110},
+                       {26, 101, 21, 0, 135, 270},
+                       {84, 9, 171, 135, 0, 191},
+                       {184, 40, 110, 270, 191, 0}};
 
-Utiliza bitmask para simular as casas ja visitadas na dfs
-Sendo S[6] = { S[0], S[1], S[2], S[3], S[4], S[5], S[6] }, com S[i] = 0 ou 1, indicando se foi visitado ou nao
-
-Distancias dos caminhos:
-*/
-/*int distancias[][N] = {  {0,10,20,30,40,20},
-						 {10,0,15,19,12,10},
-						 {20,15,0,25,14,17},
-						 {30,19,25,0,11,9},
-						 {40,12,14,11,0,10},
-					 	 {20,10,17,9,10,0}};*/
-/*int distancias[][N] = {	{0,258,172,105,250,108},
-						{258,0,30,54,145,52},
-						{172,30,0,24,176,22},
-						{105,54,24,0,245,2},
-						{250,145,176,245,0,93},
-						{108,52,22,2,93,0}};
-*/
-int distancias[][N] =  {{0,283,159,84,192},
-						{283,0,189,87,212},
-						{159,189,0,90,43},
-						{84,87,90,0,43},
-						{192,212,43,164,0}};
-// caminhos visitados com N x 2 ^ N posicoes
-int dp[N][1 << N];
-// matriz auxiliar para salvar o caminho percorrido. N x 2^N posicoes tbm
-int savePath[N][1 << N];
-
-// i = casa a verificar se ja foi visitada
-// S = bitmask do vetor de casas
-int visitada(int i, int S){
-	return (S & (1 << i));
+void swap(int *x, int *y) {
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
 }
-
-// incluir casa i (bit i = 1) no bitmask
-// retorna novo S
-int add_visitadas(int i, int S){
-	return (S | (1 << i));
+void copy_array() {
+    int i, sum = 0;
+    for (i = 0; i < N; i++) {
+        sum += distancias[a[i % N]][a[(i + 1) % N]];
+    }
+    if (cost > sum) {
+        cost = sum;
+        for (int i = 0; i < N; i++) {
+            best[i] = a[i];
+        }
+    }
 }
-
-// se todas as casas em S foram visitadas
-bool completou(int S){
-	return S == (1 << N)-1;
+void permute(int i) {
+    int j, k;
+    if (i == (N - 1)) {
+        copy_array();
+    }
+    else {
+        for (j = i; j < N; j++) {
+            swap((a + i), (a + j));
+            permute(i + 1);
+            swap((a + i), (a + j));
+        }
+    }
 }
-
-// v = vertice atual
-// S = bitmask de casas visitadas
-int caixeiro(int v, int S){
-	if(completou(S))
-		return distancias[v][0];
-
-	if(dp[v][S] != -1)
-		return dp[v][S];
-
-	int menor_caminho = 1e9;
-	for(int i=0;i<N;i++){
-		if(!visitada(i, S)){
-			int q = caixeiro(i, add_visitadas(i, S)) + distancias[v][i];
-			if(menor_caminho > q){
-				menor_caminho = q;
-				savePath[v][S] = i;
-			}
-		}
-	}
-	dp[v][S] = menor_caminho;
-	return menor_caminho;
+int main() {
+    int i, j;
+    permute(0); // arg0 = casa inicial
+    printf("Custo: %d\n", cost)
+    printf("Melhor rota: \n");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", best[i]);
+    }
+    printf("\n");
 }
-
-int main(){
-	memset(dp, -1, sizeof(dp));
-	printf("Distancia: %d\n", caixeiro(0, 1));
-	
-	int casa_atual = 0;
-	int bitmask = 1;
-	int qtd = 0;
-	printf("Caminho: 0 ");
-	do {
-		casa_atual = savePath[casa_atual][bitmask];
-		printf("%d ", casa_atual);
-		bitmask = add_visitadas(casa_atual, bitmask);
-		qtd++;
-	} while(qtd < N);
-	printf("\n");
-
-	for(int i=0;i<N;i++){
-		for(int j=0;j<(1<<N);j++){
-			printf("%d ",savePath[i][j]);
-		}
-		printf("\n");
-	}
-
-	return 0;
-}
-
-/*
-
-0001011111
-0001000000
-
-
-*/
