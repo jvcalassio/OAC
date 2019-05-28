4326059721d6d0912d2fc67989133cbd16cf4ed4
jvcalassio <39389995+jvcalassio@users.noreply.github.com>
2019-05-28 09:46:56 -0300

    Update CSRegisters.v

--- Lab3/CPU/CSRegisters.v
+++ Lab3/CPU/CSRegisters.v
@@ -4,33 +4,35 @@
 
 
 module CSRegisters (
-    input wire 			iCLK, iRST, iRegWrite, // sinal de clock, sinal de reset, se escreve no csreg ou nao
-	 input wire	[3:0]		iReadRegister, // registrador CS a ser lido
-	 input wire [3:0]		iWriteRegister, // registrador CS a ser escrito
-	 input wire [31:0]	iWriteData, // dado a ser escrito no CSR
-	
-	 output wire[31:0]	oReadData // dado lido de CSR
+    input wire 			iCLK, iRST, iCSRegWrite, // sinal de clock, sinal de reset, se escreve no csreg ou nao
+	 input wire	[6:0]		iCSReadRegister, // registrador CS a ser lido
+	 input wire [6:0]		iCSWriteRegister, // registrador CS a ser escrito
+	 input wire [31:0]	iCSWriteData, // dado a ser escrito no CSR
+		// registradores ucause e uepc vao ser escritos quase sempre
+	 input wire				iUCAUSEWrite, iUEPCWrite,
+	 input wire	[31:0]	iUCAUSEData, iUEPCData,
+	 output wire[31:0]	oCSReadData // dado lido de CSR
 	 
     //input wire  [4:0] 	iVGASelect, iRegDispSelect,
     //output reg  [31:0] 	oVGARead, oRegDisp // para mostrar no display. a ser avaliado
     );
 
 /* Control and Status Register file */
-reg [69:0] registers[31:0];
+reg [31:0] registers[69:0];
 
-reg [7:0] i;
+reg [6:0] i;
 
 initial
-	begin // seta todos os 11 registradores em 0, inicialmente
-		for (i = 0; i <= 5; i = i + 1'b1)
+	begin 
+		for (i = 0; i <= 5; i = i + 1'b1) // seta os registradores 0 a 5
 			registers[i] = 32'b1; // seta tudo em 1 pra testes apenas
 			// registers[i] = 32'b0;
-		for (i = 64; i <= 68; i = i + 1'b1)
+		for (i = 64; i <= 68; i = i + 1'b1) // seta os registradores 64 a 68
 			registers[i] = 32'b1;
 	end
 
 
-assign oReadData1 =	registers[iReadRegister1];
+assign oCSReadData =	registers[iCSReadRegister];
 
 //assign oRegDisp 	=	registers[iRegDispSelect];
 //assign oVGARead 	=	registers[iVGASelect];
@@ -43,15 +45,24 @@ assign oReadData1 =	registers[iReadRegister1];
 begin
     if (iRST) // se receber sinal de reset
     begin // reseta o banco de registradores
-        for (i = 0; i <= 10; i = i + 1'b1)
+        for (i = 0; i <= 5; i = i + 1'b1)
             registers[i] <= 32'b0;
+		  for (i = 64; i <= 68; i = i + 1'b1)
+				registers[i] <= 32'b0;
     end
     else
-	 begin // do contrario, apenas escreve dado desejado no registrador rd
+	 begin // do contrario, apenas escreve dado desejado no registrador desejado
 		i<=6'bx; // para não dar warning
-		if(iRegWrite && iWriteRegister != 5'b0)
-				registers[iWriteRegister] <= iWriteData;
-	 end
+		if(iCSRegWrite)
+				registers[iCSWriteRegister] <= iCSWriteData;
+				
+		if(iUCAUSEWrite)
+				registers[7'd66] <= iUCAUSEData;
+				
+		if(iUEPCWrite)
+				registers[7'd65] <= iUEPCData + 3'b100;
+		
+		end
 end
 
 endmodule
