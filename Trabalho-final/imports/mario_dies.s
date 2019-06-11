4cf4286d5d985b7e5c31e8ee4a87ef1241ff52c4
Joao Victor <joao.victors1203@gmail.com>
2019-06-11 15:57:19 -0300

    som de morte

--- Trabalho-final/imports/mario_dies.s
+++ Trabalho-final/imports/mario_dies.s
@@ -2,21 +2,21 @@
 .data
 # Numero de Notas a tocar
 NUM: .word 4
-# lista de nota,duração,nota,duração,nota,duração,...
-NOTAS: 64,300,67,300,55,300,60,400,
+# lista de nota,duraï¿½ï¿½o,nota,duraï¿½ï¿½o,nota,duraï¿½ï¿½o,...
+NOTAS: 64,200,67,300,55,300,60,400,
 
 .text
 	li a7,30
 	ecall
 	mv t1,a0		# salva o tempo inicial
-	la s0,NUM		# define o endereço do número de notas
+	la s0,NUM		# define o endereï¿½o do nï¿½mero de notas
 	lw s1,0(s0)		# le o numero de notas
-	la s0,NOTAS		# define o endereço das notas
+	la s0,NOTAS		# define o endereï¿½o das notas
 	li t0,0			# zera o contador de notas
-	li a2,80		# define o instrumento
+	li a2,81		# define o instrumento
 	li a3,127		# define o volume
 
-LOOP:	beq t0,s1, FIM		# contador chegou no final? então vá para FIM
+LOOP:	beq t0,s1, FIM		# contador chegou no final? entï¿½o vï¿½ para FIM
 	lw a0,0(s0)		# le o valor da nota
 	lw a1,4(s0)		# le a duracao da nota
 	add t3,t1,a1		# soma duracao total da nota com o tempo
@@ -29,7 +29,7 @@ LOOP2:	li a7,30
 	li a7,31		# define a chamada de syscall
 	ecall			# toca a nota
 	addi t0,t0,1		# incrementa o contador de notas
-	addi s0,s0,8		# incrementa para o endereço da próxima nota
+	addi s0,s0,8		# incrementa para o endereï¿½o da prï¿½xima nota
 	mv t1,t3		# novo tempo inicial = soma da duracao da nota com tempo inicial
 	j LOOP			# volta ao loop
 
---
+++ Trabalho-final/sounds/mario_sounds.s
@@ -0,0 +1,2 @@
+mario_death_num: .word 4
+mario_death: .word 64,300,67,300,55,350,60,300,0,40
