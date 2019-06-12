46abb51c2bf0ca0415d4b19ab685c84814e4088f
João Coutinho <joaopedro0801@hotmail.com>
2019-06-12 13:33:30 -0300

    pixel2.s quase pronto, falta botao para colorir

--- Lab3/Docs/System_Rars/pixel2.s
+++ Lab3/Docs/System_Rars/pixel2.s
@@ -1,7 +1,22 @@
+# salva conteudo de REG na pilha
+.macro save_stack(%reg)
+	addi sp,sp,-4
+	sw %reg,0(sp)
+.end_macro
+
+# libera algo da pilha e salva em REG
+.macro free_stack(%reg)
+	lw %reg,0(sp)
+	addi sp,sp,4
+.end_macro
+
+
 .include "macros2.s"
+
 
 .eqv preto 0x00
 .eqv branco 0xFF
+
 
 .text
 
@@ -19,369 +34,476 @@ MAIN:
 	sb	t0, 0(a0)	#a0 = posicao do pixel
 
 	MAINLOOP:
-		li	a0, 100
+		li	a0, 50
 		li	a7, 32
-		ecall		#sleep de 125ms
+		ecall		#sleep de 50
 
 		li	t0, AdcCH0
-		lw	s2, 0(t0)	#s2 = x, x entre 0x000 e 0xFFF
+		lw	s2, 0(t0)	#s2 = x do analogico, x entre 0x000 e 0xFFF
 		
 		li	t0, 100
-		bleu	s2, t0, LEFT3	#x < 100 ? goto LEFT3
-		
+		bleu	s2, t0, gotoLEFT3	#x < 100 ? goto LEFT3
+		continue0:
 		li	t0, 1000
-		bleu	s2, t0, LEFT2	#x<1000 ? goto LEFT2
-		
+		bleu	s2, t0, gotoLEFT2	#x<1000 ? goto LEFT2
+		continue1:
 		li	t0, 2400
-		bleu	s2, t0, LEFT1	#x<2400 ? goto LEFT1
-		
+		bleu	s2, t0, gotoLEFT1	#x<2400 ? goto LEFT1
+		continue2:
 		li	t0, 3300
-		bleu	s2, t0, RIGHT1	#x<3300 ? goto RIGHT1
-		
+		bleu	s2, t0, gotoRIGHT1	#x<3300 ? goto RIGHT1
+		continue3:
 		li	t0, 4000
-		bleu	s2, t0, RIGHT2	#x < 4000 ? goto RIGHT2
-		
+		bleu	s2, t0, gotoRIGHT2	#x < 4000 ? goto RIGHT2
+		continue4:
 		li	t0, 5000
-		bleu	s2, t0, RIGHT3	#x < 5000? goto RIGHT3
+		bleu	s2, t0, gotoRIGHT3	#x < 5000? goto RIGHT3
 		
-		
+		continue5:
 		li	t0, AdcCH4
-		lw	s2, 0(t0)	#s2 = y, y entre 0x000 e 0xFFF
+		lw	s3, 0(t0)	#s3 = y do analogico, y entre 0x000 e 0xFFF
 		
 		li	t0, 100
-		bleu	s2, t0, DOWN3	#y < 100 ? goto DOWN3
-		
-		li	t0, 1000
-		bleu	s2, t0, DOWN2	#y<1000 ? goto DOWN2
-		
+		bleu	s3, t0, gotoUP3	#y < 100 ? goto UP3
+		continue6:
+		li	t0, 1000	
+		bleu	s3, t0, gotoUP2	#y<1000 ? goto UP2
+		continue7:
 		li	t0, 2400
-		bleu	s2, t0, DOWN1	#y<2400 ? goto DOWN1
-		
+		bleu	s3, t0, gotoUP1	#y<2400 ? goto UP1
+		continue8:
 		li	t0, 3300
-		bleu	s2, t0, UP1	#x<3300 ? goto UP1
-		
+		bleu	s3, t0, gotoDOWN1	#y<3300 ? gotoDOWN1
+		continue9:
 		li	t0, 4000
-		bleu	s2, t0, UP2	#x < 4000 ? goto UP2
-		
+		bleu	s3, t0, gotoDOWN2	#y < 4000 ? goto DOWN2
+		continue10:
 		li	t0, 5000
-		bleu	s2, t0, UP3	#x < 5000? goto UP3
-		
+		bleu	s3, t0, gotoDOWN3	#y < 5000? goto DOWN3
+		continue11:
 		j MAINLOOP
+		
+		
+		gotoLEFT3:
+			jal	LEFT3
+			j	continue0
+		
+		gotoLEFT2:
+			jal	LEFT2
+			j	continue1
+		
+		gotoLEFT1:
+			jal	LEFT1
+			j	continue2
+		
+		gotoRIGHT1:
+			jal	RIGHT1
+			j	continue3
+		
+		gotoRIGHT2:
+			jal 	RIGHT2
+			j	continue4
+		
+		gotoRIGHT3:
+			jal	RIGHT3
+			j	continue5
+		
+		gotoUP3:
+			jal	UP3
+			j	continue6
+		
+		gotoUP2:
+			jal	UP2
+			j	continue7
+		
+		gotoUP1:
+			jal	UP1
+			j	continue8
+		
+		gotoDOWN1:
+			jal	DOWN1
+			j	continue9
+		
+		gotoDOWN2:
+			jal 	DOWN2
+			j	continue10
+		
+		gotoDOWN3:
+			jal	DOWN3
+			j	continue11
+		
 		
 		LEFT3:
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, preto
 			sb	t0, 0(a0)	#preenche a posição atual com preto
 			
-			addi	s0, s0, -9	#move o pixel tres posicoes para a esquerda
+			addi	s0, s0, -9	#move o pixel nove posicoes para a esquerda
 			
 			li	t1, 1
 			ble	s0, t1, LIMIT_LEFT
 			
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, branco
 			sb	t0, 0(a0)	#preenche a nova posicao com branco
-			j	 MAINLOOP
+			ret
 			
 		LEFT2:
 			li	t0, 100
 			bgeu	s2, t0, LEFT2continue	#x > 100 ? continue
-			j	 MAINLOOP
+			ret
 			
 		LEFT2continue:
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, preto
 			sb	t0, 0(a0)	#preenche a posição atual com preto
 			
-			addi	s0, s0, -3	#move o pixel dois posicoes para a esquerda
+			addi	s0, s0, -3	#move o pixel tres posicoes para a esquerda
 			
 			li	t1, 1
 			ble	s0, t1, LIMIT_LEFT
 			
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, branco
 			sb	t0, 0(a0)	#preenche a nova posicao com branco
-			j 	MAINLOOP
+			ret
 			
 		LEFT1:
 			li	t0, 1000
 			bgeu	s2, t0, LEFT1continue	#x > 1000 ? continue
-			j	 MAINLOOP
+			ret
 		
 		LEFT1continue:
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, preto
 			sb	t0, 0(a0)	#preenche a posição atual com preto
 			
-			addi	s0, s0, -1	#move o pixel tres posicoes para a esquerda
+			addi	s0, s0, -1	#move o pixel uma posicao para a esquerda
 			
 			li	t1, 1
 			ble	s0, t1, LIMIT_LEFT
 			
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, branco
 			sb	t0, 0(a0)	#preenche a nova posicao com branco
-			j 	MAINLOOP
+			ret
 		
 		LIMIT_LEFT:
 			li	s0, 1
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, branco
 			sb	t0, 0(a0)	#preenche a nova posicao com branco
-			j 	MAINLOOP			
+			ret			
 		
 		RIGHT1:
 			li	t0, 2500
 			bgeu	s2, t0, RIGHT1continue	#x > 2500 ? continue
-			j	MAINLOOP
+			ret
 		
 		RIGHT1continue:
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal	 GET_POSITION
+			free_stack(ra)
 			li	t0, preto
 			sb	t0, 0(a0)	#preenche a pos atual com preto
 			
-			addi	s0, s0, 1
+			addi	s0, s0, 1	#move o pixel uma pos para a direita
 			
 			li	t1, 319
 			bge	s0, t1, LIMIT_RIGHT
 			
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal	GET_POSITION
+			free_stack(ra)
 			li	t0, branco
 			sb	t0, 0(a0)
-			j	MAINLOOP
+			ret
 		
 		RIGHT2:
 			li	t0, 3300
-			bgeu	s2, t0, RIGHT2continue	#x > 3000? continue
-			j	MAINLOOP
+			bgeu	s2, t0, RIGHT2continue	#x > 3300? continue
+			ret
 		
 		RIGHT2continue:
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal	GET_POSITION
-			li	t0, preto
-			sb	t0, 0(a0)
+			free_stack(ra)
+			li	t0, preto	
+			sb	t0, 0(a0)	#preenche a pos atual com preto
 			
-			addi	s0,s0, 3
+			addi	s0,s0, 3	#move o pixel 3 pos para a direita
 			
 			li	t1, 319
 			bge	s0, t1, LIMIT_RIGHT
 			
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal	GET_POSITION
+			free_stack(ra)
 			li	t0, branco
 			sb	t0,0(a0)
-			j	MAINLOOP
+			ret
 		
 		RIGHT3:
 			li	t0, 4000
-			bgeu	s2, t0, RIGHT3continue
-			j	MAINLOOP
+			bgeu	s2, t0, RIGHT3continue	#x>4000? continue
+			ret
 		
 		RIGHT3continue:
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal	GET_POSITION
-			li	t0, preto
+			free_stack(ra)
+			li	t0, preto	#preenche a pos atual com preto
 			sb	t0,0(a0)
 			
-			addi	s0,s0,9
+			addi	s0,s0,9		#move o pixel nove pos para a direita
 			li	t1, 319
 			bge	s0, t1, LIMIT_RIGHT
 			
 			mv	a0,s0
 			mv	a1, s1
+			save_stack(ra)
 			jal	GET_POSITION
+			free_stack(ra)
 			li	t0, branco
 			sb	t0, 0(a0)
-			j	MAINLOOP
+			ret
 			
 		LIMIT_RIGHT:
 			li	s0, 319
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal	GET_POSITION
+			free_stack(ra)
 			li	t0, branco
 			sb	t0, 0(a0)
-			j	MAINLOOP
+			ret
 		
 		
 		
-		DOWN3:
+		UP3:
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, preto
 			sb	t0, 0(a0)	#preenche a posição atual com preto
 			
-			addi	s1, s1, 9	#move o pixel tres posicoes para baixo
+			addi	s1, s1, -9	#move o pixel nove posicoes para cima
 			
-			li	t1, 239
-			bge	s1, t1, LIMIT_DOWN
+			li	t1, 1
+			ble	s1, t1, LIMIT_UP
 			
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, branco
 			sb	t0, 0(a0)	#preenche a nova posicao com branco
-			j	 MAINLOOP
+			ret
 			
-		DOWN2:
+		UP2:
 			li	t0, 100
-			bgeu	s2, t0, DOWN2continue	#x > 100 ? continue
-			j	 MAINLOOP
+			bgeu	s3, t0, UP2continue	#t > 100 ? continue
+			ret
 			
-		DOWN2continue:
+		UP2continue:
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, preto
 			sb	t0, 0(a0)	#preenche a posição atual com preto
 			
-			addi	s1, s1, 3	#move o pixel dois posicoes para a esquerda
+			addi	s1, s1, -3	#move o pixel tres posicoes para cima
 			
-			li	t1, 239
-			bge	s1, t1, LIMIT_DOWN
+			li	t1, 1
+			ble	s1, t1, LIMIT_UP
 			
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
 			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, branco
 			sb	t0, 0(a0)	#preenche a nova posicao com branco
-			j 	MAINLOOP
+			ret
 			
-		DOWN1:
-			li	t0, 1000
-			bgeu	s2, t0, DOWN1continue	#x > 1000 ? continue
-			j	 MAINLOOP
-		
-		DOWN1continue:
-			mv	a0, s0
-			mv	a1, s1
-			jal 	GET_POSITION
-			li	t0, preto
-			sb	t0, 0(a0)	#preenche a posição atual com preto
-			
-			addi	s1, s1, 1	#move o pixel tres posicoes para a esquerda
-			
-			li	t1, 239
-			bge	s1, t1, LIMIT_DOWN
-			
-			mv	a0, s0
-			mv	a1, s1
-			jal 	GET_POSITION
-			li	t0, branco
-			sb	t0, 0(a0)	#preenche a nova posicao com branco
-			j 	MAINLOOP
-		
-		LIMIT_DOWN:
-			li	s1, 239
-			mv	a0, s0
-			mv	a1, s1
-			jal 	GET_POSITION
-			li	t0, branco
-			sb	t0, 0(a0)	#preenche a nova posicao com branco
-			j 	MAINLOOP			
-		
 		UP1:
-			li	t0, 2500
-			bgeu	s2, t0, UP1continue	#x > 2500 ? continue
-			j	MAINLOOP
+			li	t0, 1000
+			bgeu	s3, t0, UP1continue	#y > 1000 ? continue
+			ret
 		
 		UP1continue:
 			mv	a0, s0
 			mv	a1, s1
-			jal	 GET_POSITION
+			save_stack(ra)
+			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, preto
-			sb	t0, 0(a0)	#preenche a pos atual com preto
+			sb	t0, 0(a0)	#preenche a posição atual com preto
 			
-			addi	s1, s1, -1
+			addi	s1, s1, -1	#move o pixel uma pos para cima
 			
 			li	t1, 1
 			ble	s1, t1, LIMIT_UP
 			
 			mv	a0, s0
 			mv	a1, s1
-			jal	GET_POSITION
+			save_stack(ra)
+			jal 	GET_POSITION
+			free_stack(ra)
 			li	t0, branco
-			sb	t0, 0(a0)
-			j	MAINLOOP
+			sb	t0, 0(a0)	#preenche a nova posicao com branco
+			ret
 		
-		UP2:
-			li	t0, 3300
-			bgeu	s2, t0, UP2continue	#x > 3000? continue
-			j	MAINLOOP
-		
-		UP2continue:
-			mv	a0, s0
-			mv	a1, s1
-			jal	GET_POSITION
-			li	t0, preto
-			sb	t0, 0(a0)
-			
-			addi	s1,s1, -3
-			
-			li	t1, 1
-			ble	s1, t1, LIMIT_UP
-			
-			mv	a0, s0
-			mv	a1, s1
-			jal	GET_POSITION
-			li	t0, branco
-			sb	t0,0(a0)
-			j	MAINLOOP
-		
-		UP3:
-			li	t0, 4000
-			bgeu	s2, t0, UP3continue
-			j	MAINLOOP
-		
-		UP3continue:
-			mv	a0, s0
-			mv	a1, s1
-			jal	GET_POSITION
-			li	t0, preto
-			sb	t0,0(a0)
-			
-			addi	s1,s1,-9
-			
-			li	t1, 1
-			ble	s1, t1, LIMIT_UP
-			
-			mv	a0,s0
-			mv	a1, s1
-			jal	GET_POSITION
-			li	t0, branco
-			sb	t0, 0(a0)
-			j	MAINLOOP
-	
 		LIMIT_UP:
 			li	s1, 1
 			mv	a0, s0
 			mv	a1, s1
+			save_stack(ra)
+			jal 	GET_POSITION
+			free_stack(ra)
+			li	t0, branco
+			sb	t0, 0(a0)	#preenche a nova posicao com branco
+			ret			
+		
+		DOWN1:
+			li	t0, 2600
+			bgeu	s3, t0, DOWN1continue	#y > 2600 ? continue
+			ret
+		
+		DOWN1continue:
+			mv	a0, s0
+			mv	a1, s1
+			save_stack(ra)
 			jal	GET_POSITION
+			free_stack(ra)
+			li	t0, preto
+			sb	t0, 0(a0)	#preenche a pos atual com preto
+			
+			addi	s1, s1, 1	#move o pixel uma pos para baixo
+			
+			li	t1, 239
+			bge	s1, t1, LIMIT_DOWN
+			
+			mv	a0, s0
+			mv	a1, s1
+			save_stack(ra)
+			jal	GET_POSITION
+			free_stack(ra)
 			li	t0, branco
 			sb	t0, 0(a0)
-			j	MAINLOOP
+			ret
+		
+		DOWN2:
+			li	t0, 3300
+			bgeu	s3, t0, DOWN2continue	#y > 3300? continue
+			ret
+		
+		DOWN2continue:
+			mv	a0, s0
+			mv	a1, s1
+			save_stack(ra)
+			jal	GET_POSITION
+			free_stack(ra)
+			li	t0, preto
+			sb	t0, 0(a0)
+			
+			addi	s1,s1, 3	#move o pixel tres pos para baixo
+			
+			li	t1, 239
+			bge	s1, t1, LIMIT_DOWN
+			
+			mv	a0, s0
+			mv	a1, s1
+			save_stack(ra)
+			jal	GET_POSITION
+			free_stack(ra)
+			li	t0, branco
+			sb	t0,0(a0)
+			ret
+		
+		DOWN3:
+			li	t0, 4000
+			bgeu	s3, t0, DOWN3continue	#y > 4000? continue
+			ret
+		
+		DOWN3continue:
+			mv	a0, s0
+			mv	a1, s1
+			save_stack(ra)
+			jal	GET_POSITION
+			free_stack(ra)
+			li	t0, preto
+			sb	t0,0(a0)
+			
+			addi	s1,s1,9		#move nove pos para baixo
+			
+			li	t1, 239
+			bge	s1, t1, LIMIT_DOWN
+			
+			mv	a0,s0
+			mv	a1, s1
+			save_stack(ra)
+			jal	GET_POSITION
+			free_stack(ra)
+			li	t0, branco
+			sb	t0, 0(a0)
+			ret
+		
+		#impede que pixel ultrapasee o limite inferior da tela
+		LIMIT_DOWN:
+			li	s1, 239
+			mv	a0, s0
+			mv	a1, s1
+			save_stack(ra)
+			jal	GET_POSITION
+			free_stack(ra)
+			li	t0, branco
+			sb	t0, 0(a0)
+			ret
 
 #a0 = x	
 #a1 = y
