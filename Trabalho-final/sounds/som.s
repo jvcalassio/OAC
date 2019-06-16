0f7e14de9b8dcb5c6409953528eadffb2b6bfa2f
Vinicius Rocha <43426385+vinicius-s-rocha@users.noreply.github.com>
2019-06-16 18:46:15 -0300

    Add files via upload
    Adicionado os seguintes sons: 
    -Mario morrendo
    -Passos do Mario, 
    -Comeco de Fase
    -Final de Fase
    -Musica Tema Fase 1 e 2
    -Musica Tema Fase 3

---
+++ Trabalho-final/sounds/som.s
@@ -0,0 +1,42 @@
+.data
+# numero de Notas a tocar
+NUM_MARIOMORRE: .word 16
+# lista de nota,duração,nota,duração,nota,duração,...
+NOTAS_MARIOMORRE: 	
+	58,0,65,0,62,0,68,0,60,0,67,0,59,0,66,0,58,0,65,0,56,0,63,0, 	# a2 = 74		
+	64,300,67,300,55,300,60,300 					# a2 = 80
+	
+# numero de Notas a tocar	
+NUM_NIVEL1: .word 6
+# lista de nota,duração,nota,duração,nota,duração,...
+NOTAS_NIVEL1: 								# a2 = 80
+	48,300, # pausa de 300 milisegundos
+	52,300,55,300,57,300,55,300 # tocar em looping durante o jogo
+
+# numero de Notas a tocar
+NUM_NIVEL3: .word 3
+# lista de nota,duração,nota,duração,nota,duração,...
+NOTAS_NIVEL3: 								# a2 = 80
+	48,80,48,80,52,80, # Tocar em looping e antes de iniciar um novo looping uma pausa de 100 milisegundos
+	
+# numero de Notas a tocar
+NUM_FASE_START: .word 8
+# lista de nota,duração,nota,duração,nota,duração,...
+NOTAS_FASE_START: 	
+	67,300,69,300,72,300,69,300,67,300,69,300,65,350,53,300 	# a2 = 80
+	
+# numero de Notas a tocar
+NUM_FASE_CLEAR: .word 11
+# lista de nota,duração,nota,duração,nota,duração,...
+NOTAS_FASE_CLEAR: 							# a2 = 80
+	60,350,60,350,60,450,67,300,69,300,71,300,67,450,67,300,69,300,71,300,67,450 
+	
+# numero de Notas a tocar
+NUM_PASSOS: .word 4
+# lista de nota,duração,nota,duração,nota,duração,...
+NOTAS_PASSOS: 								# a2 = 120
+	62,30,65,30,62,30,69,30, # Tocar em looping
+
+
+
+.text
